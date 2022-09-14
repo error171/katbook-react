3b363f6fad7afb673ff7c9eeaf1b10efa7f3f01b
error171 <dqngthng@gmail.com>
2022-09-14 17:19:53 +0700

    .

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const request = axios.create({
+  baseURL: `https://api.mangadex.org/`,
   headers: {
     "content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
