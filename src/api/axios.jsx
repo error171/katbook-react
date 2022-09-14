de4b03322b0e88e2c8317061f8a1427866979f6e
error171 <dqngthng@gmail.com>
2022-09-14 17:00:06 +0700

    .

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const request = axios.create({
-  baseURL: `https://api.mangadex.org/`,
   headers: {
     "content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,7 @@ const { proxyMiddleware } = require("http-proxy-middleware");
 module.exports = (app) => {
   app.use(
     proxyMiddleware("/manga", {
-      target: "https://api.mangadex.org/",
+      target: "https://api.mangadex.org",
       changeOrigin: true,
     })
   );
