7d6ec7aa395ac56abf9c14cfae6103db0e002a96
error171 <dqngthng@gmail.com>
2022-09-14 16:41:30 +0700

    commit

---
+++ src/proxy.js
@@ -0,0 +1,8 @@
+const { proxyMiddleware } = require("http-proxy-middleware");
+module.exports = (app) => {
+  app.use(
+    proxyMiddleware('/manga',{
+        target: 'https://api.mangadex.org',
+        changeOrigin: true
+    }));
+};
